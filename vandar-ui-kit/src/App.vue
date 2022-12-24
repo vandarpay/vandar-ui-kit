d8c7a267a308afb9fe0fd9c18ab3d8819c9eac92
fahimekhandan <fahimekhandan@gmail.com>
2022-12-24 12:35:51 +0330

    feat: working on button component

--- vandar-ui-kit/src/App.vue
+++ vandar-ui-kit/src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
     <newTest></newTest>
+    <baseBtn color="primary" label="متن دکمه" type="text"></baseBtn>
   </div>
 </template>
 
 <script setup>
 import newTest from './components/NewTest/index.vue'
+import baseBtn from './components/BaseBtn/index.vue'
 
 </script>
 
