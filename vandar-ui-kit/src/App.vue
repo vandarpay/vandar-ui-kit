556f4b68e0460bf85784d01d2c3e74b29d17f86c
fahimekhandan <fahimekhandan@gmail.com>
2022-12-24 17:19:19 +0330

    feat: added white and neutral colors to btn

--- vandar-ui-kit/src/App.vue
+++ vandar-ui-kit/src/App.vue
@@ -1,16 +1,47 @@
 <template>
-  <div>
+  <div class="p-12">
     <newTest></newTest>
-    <vanButton class="btn btn-teal btn-larg" label="متن دکمه"></vanButton>
+    <vanButton class="btn btn-teal btn-large" label="متن دکمه"></vanButton>
     <div class="pt-12"></div>
     <vanButton class="btn btn-teal" label="متن دکمه"></vanButton>
     <div class="pt-12"></div>
     <vanButton class="btn btn-teal btn-small" label="متن دکمه"></vanButton>
     <div class="pt-12"></div>
-    <vanButton class="btn btn-teal btn-outline" label="متن دکمه"></vanButton>
+    <vanButton class="btn btn-teal btn-outline btn-large" label="متن دکمه"></vanButton>
     <div class="pt-12"></div>
-
     <vanButton class="btn btn-teal btn-text" label="متن دکمه"></vanButton>
+    <div class="pt-12"></div>
+    <vanButton class="btn btn-red btn-large" label="متن دکمه"></vanButton>
+    <div class="pt-12"></div>
+    <vanButton class="btn btn-red" label="متن دکمه"></vanButton>
+    <div class="pt-12"></div>
+    <vanButton class="btn btn-red btn-small" label="متن دکمه"></vanButton>
+    <div class="pt-12"></div>
+    <vanButton class="btn btn-red btn-outline" label="متن دکمه"></vanButton>
+    <div class="pt-12"></div>
+    <vanButton class="btn btn-red btn-text" label="متن دکمه"></vanButton>
+    <div class="pt-12"></div>
+    <vanButton class="btn btn-neutral btn-large" label="متن دکمه"></vanButton>
+    <div class="pt-12"></div>
+    <vanButton class="btn btn-neutral" label="متن دکمه"></vanButton>
+    <div class="pt-12"></div>
+    <vanButton class="btn btn-neutral btn-small" label="متن دکمه"></vanButton>
+    <div class="pt-12"></div>
+    <vanButton class="btn btn-neutral btn-outline btn-small" label="متن دکمه"></vanButton>
+    <div class="pt-12"></div>
+    <vanButton class="btn btn-neutral btn-text" label="متن دکمه"></vanButton>
+    <div class="pt-12"></div>
+<div class="bg-neutral-900 p-24">
+  <vanButton class="btn btn-white btn-large" label="متن دکمه"></vanButton>
+    <div class="pt-12"></div>
+    <vanButton class="btn btn-white" label="متن دکمه"></vanButton>
+    <div class="pt-12"></div>
+    <vanButton class="btn btn-white btn-small" label="متن دکمه"></vanButton>
+    <div class="pt-12"></div>
+    <vanButton class="btn btn-white btn-outline" label="متن دکمه"></vanButton>
+    <div class="pt-12"></div>
+    <vanButton class="btn btn-white btn-text" label="متن دکمه"></vanButton>
+</div>
   </div>
 </template>
 
