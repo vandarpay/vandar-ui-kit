4b8294c9b6534fe4b220142083d72de0ba34880b
fahimekhandan <fahimekhandan@gmail.com>
2022-12-25 16:20:39 +0330

    feat: add storybook for btn

---
+++ vandar-ui-kit/src/components/vanButton/index.stories.js
@@ -0,0 +1,43 @@
+import vanButton from './index.vue';
+
+export default {
+  title: 'Components/button',
+  component: vanButton,
+  // argTypes
+  argTypes: {
+    onClick: {},
+    color: {
+      control: 'select' ,
+      options: ["teal" , "neutral" , "red",  "white"],
+    },
+    size: {
+      control: 'select' ,
+      options: ['small', 'medium', 'large'],
+    },
+    type: {
+      control: 'select' ,
+      options: ["fill" , "ghost" , "outline"],
+    },
+  },
+};
+
+// templates
+const Template = (args) => ({
+  // Components 
+  components: { vanButton },
+  //  map
+  setup() {
+    return { args };
+  },
+  // bound
+  template: '<van-button v-bind="args" />',
+});
+
+export const button = Template.bind({});
+// args
+button.args = {
+  disabled: false,
+  loading: false,
+  label: 'متن دکمه',
+};
+
