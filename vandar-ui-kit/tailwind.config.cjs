23d702d0e2825812be8e49fbe193c5b0eb5d41de
Kasra Dara <kasradara@outlook.com>
2022-12-26 19:00:25 +0330

    build tailwind presets to use

--- vandar-ui-kit/tailwind.config.cjs
+++ vandar-ui-kit/tailwind.config.cjs
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-const { colors } = require('./src/designTokens/colors.cjs');
-const { fontSize } = require('./src/designTokens/fontSize.cjs');
-const { spacing } = require('./src/designTokens/spacing.cjs');
+const {
+  colors
+} = require('./designTokens/colors.cjs');
+const {
+  fontSize
+} = require('./designTokens/fontSize.cjs');
+const {
+  spacing
+} = require('./designTokens/spacing.cjs');
 
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
@@ -10,5 +16,4 @@ module.exports = {
     fontSize,
     spacing
   },
-  plugins: [require("prettier-plugin-tailwindcss")],
 };
